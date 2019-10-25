bc4192a9b4a43151bd8b1e5fb07ef85cdb34a377
Rosalis Freebairn <rfreeb@artic.edu>
2019-10-24 19:05:27 -0500

    Make the server functional displaying the home page and survey page.

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+let friendArray = [
+  {
+    name: "Christian",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+
+  {
+    name: "Jason",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [3, 2, 4, 1, 5, 1, 3, 5, 1, 1]
+  },
+
+  {
+    name: "Nick",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 3, 4, 5, 2, 1, 2, 1, 1, 5]
+  },
+
+  {
+    name: "Dan",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [3, 5, 2, 5, 3, 2, 1, 4, 4, 3]
+  },
+
+  {
+    name: "Jesus",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [4, 3, 2, 3, 5, 2, 5, 3, 4, 3]
+  }
+];
---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const path = require("path");
+const htmlRoutes = require("./app/routing/htmlRoutes");
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("./app/public"));
+
+// require("/app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+
+// const server = http.createServer(handleRequest);
+
+// // Start our server
+// server.listen(PORT, function() {
+//   // Callback triggered when server is successfully listening. Hurray!
+//   console.log("Server listening on: http://localhost:" + PORT);
+// });
+
+// // Create a function which handles incoming requests and sends responses
+// function handleRequest(req, res) {
+//   // Capture the url the request is made to
+//   var path = req.url;
+
+//   // Depending on the URL, display a different HTML file.
+//   switch (path) {
+//     case "/":
+//       return displayRoot(res);
+
+//     case "/survey":
+//       return displaySurvey(res);
+
+//     default:
+//       return display404(path, res);
+//   }
+// }
+
+// function displayRoot(res) {
+//   // Configure the response to return a status code of 200 (meaning everything went OK), and to be an HTML document
+//   res.writeHead(200, { "Content-Type": "text/html" });
+
+//   // End the response by sending the client the myHTML string (which gets rendered as an HTML document thanks to the code above)
+//   res.end(myHTML);
+// }
+
+// // When someone visits the "http://localhost:8080/Survey" path, this function is run.
+// function displaySurvey(res) {
+//   // Configure the response to return a status code of 200 (meaning everything went OK), and to be an HTML document
+//   res.writeHead(200, { "Content-Type": "text/html" });
+
+//   // End the response by sending the client the myHTML string (which gets rendered as an HTML document thanks to the code above)
+//   res.end(myHTML);
+// }
+
+// // When someone visits any path that is not specifically defined, this function is run.
+// function display404(url, res) {
+//   var myHTML =
+//     "<html>" +
+//     "<body><h1>404 Not Found </h1>" +
+//     "<p>The page you were looking for: " +
+//     url +
+//     " can not be found</p>" +
+//     "</body></html>";
+
+//   // Configure the response to return a status code of 404 (meaning the page/resource asked for couldn't be found), and to be an HTML document
+//   res.writeHead(404, { "Content-Type": "text/html" });
+
+//   // End the response by sending the client the myHTML string (which gets rendered as an HTML document thanks to the code above)
+//   res.end(myHTML);
+// }
